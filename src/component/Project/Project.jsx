07d4f8a54cd73b5a8b2b2a4f79a406f5c20a321c
sopyaylatt2003 <sopyaylatt.digitalzeekwet@gmail.com>
2024-01-03 22:28:15 +0630

    doing

--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import Project03 from "./Project03";
+// import Project03 from "./Project03";
 import LineWrapper from "../UI/LineWrapper";
 import Header from "../UI/Header";
 import ToDiscuss from "../Contact/ToDiscuss";
 import Footer from "../Contact/Footer";
+import Project02 from "./Project02";
 
 const Project = () => {
   return (
     <div className="min-h-screen">
       <LineWrapper />
       <Header />
-      <Project03 />
+      {/* <Project03 /> */}
+      <Project02 />
       <ToDiscuss />
       <Footer />
     </div>
---
+++ src/component/Project/Project02.jsx
@@ -0,0 +1,215 @@
+import React from "react";
+
+const Project02 = () => {
+  return (
+    <div>
+      <section className="py-[120px]">
+        <div className="container">
+          <div className="grid grid-cols-12 mb-[1.5rem]">
+            <div className="col-span-12 md:col-span-4 mb-[30px] px-3">
+              <div
+                className="relative text-xs leading-3 tracking-[3px] font-normal text-[#1b1b1b] border-bottom 
+             flex items-center pt-[5px] pb-[15px] ps-[15px] mt-[5px]"
+              >
+                {" "}
+                <span
+                  className="before:content-[''] top-[4o%] block w-[5px] h-[5px] absolute left-0 rounded-full 
+            bg-[#c5a47e] "
+                ></span>
+                DISCOVER
+              </div>
+            </div>
+
+            <div className="col-span-12 md:col-span-8 px-3">
+              <div className="font-heading text-[#1b1b1b] text-[36px] font-normal mb-5 py-2.5 leading-6 tracking-[3px] [word-spacing:5px]">
+                CREATIVE{" "}
+                <span className="text-[#c5a47e] tracking-[3px]">PROJECTS</span>
+              </div>
+              <p className="text-[15px] leading-7 text-[#777] font-normal mb-5">
+                Architecture viverra tristique justo duis vitae diaminte neque
+                nivamus aestan ateuene artines ariianu the ateliten finibus
+                viverra nec lacus in the nedana mis erodino. Design nila iman
+                the finise viverra nec a lacus miss viventa in the setlien
+                suscipe no curabit tristue the seneoice misuscipit non sagie the
+                fermen.
+              </p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-12 text-center ">
+            <ul className="col-span-12 mx-auto relative pb-[30px] w-full">
+              <li className="inline-block text-[15px] me-[15px] cursor-pointer leading-7 text-[#c5a47e] border-b">
+                All
+              </li>
+              <li className="inline-block text-[15px] me-[15px] cursor-pointer leading-7 text-[#777]">
+                Ongoing Projects
+              </li>
+              <li className="inline-block text-[15px] me-[15px] cursor-pointer leading-7 text-[#777]">
+                Completed Projects
+              </li>
+            </ul>
+          </div>
+
+          <div className="grid grid-cols-12 relative h-[1618.21px]">
+            <div className="col-span-6 resolute left-0  top-0">
+              <div className="relative ps-[90px] pb-[90px] group">
+                <a href="#" className="text-[#c5a47e] ">
+                  <img
+                    className="w-full h-auto group-hover:scale-95 duration-500 ease-in-out "
+                    src="https://duruthemes.com/demo/html/archsan/light/images/projects/06.jpg"
+                    alt=""
+                  />
+                </a>
+                <div className="absolute left-[40px] top-[80px]">
+                  <p className="mb-[5px] tracking-[3px] leading-7 font-normal text-[12px] text-[#c5a47e]">
+                    EXTERIOR
+                  </p>
+                  <h3 className="text-2xl tracking-[3px] leading-7 font-normal mb-4 font-heading text-[#c5a47e]">
+                    <a href="">MODERN HOUSES</a>
+                  </h3>
+                  <a
+                    href=""
+                    className="text- [#fff] font-normal relative pe-[40px] leading-7 inline-block ps-[80px] text-[16px]"
+                  >
+                    <span className="before:content-[''] w-[80px] h-[1px] inline-block absolute left-0 bottom-[15px] bg-[#1b1b1b]"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-6 resolute left-[570px] top-0  mt-[75px]">
+              <div className="relative ps-[90px] pb-[90px] group">
+                <a href="#" className="text-[#c5a47e] ">
+                  <img
+                    className="w-full h-auto group-hover:scale-95 duration-500 ease-in-out "
+                    src="https://duruthemes.com/demo/html/archsan/light/images/projects/02.jpg"
+                    alt=""
+                  />
+                </a>
+                <div className="absolute left-[40px] top-[80px]">
+                  <p className="mb-[5px] tracking-[3px] leading-7 font-normal text-[12px] text-[#c5a47e]">
+                    DESIGN
+                  </p>
+                  <h3 className="text-2xl tracking-[3px] leading-7 font-normal mb-4 font-heading text-[#c5a47e]">
+                    <a href="">LUXURIOUS VILLA</a>
+                  </h3>
+                  <a
+                    href=""
+                    className="text- [#fff] font-normal relative pe-[40px] leading-7 inline-block ps-[80px] text-[16px]"
+                  >
+                    <span className="before:content-[''] w-[80px] h-[1px] inline-block absolute left-0 bottom-[15px] bg-[#1b1b1b]"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-6 resolute left-[570px] top-[520px] ">
+              <div className="relative ps-[90px] pb-[90px] group">
+                <a href="#" className="text-[#c5a47e] ">
+                  <img
+                    className="w-full h-auto group-hover:scale-95 duration-500 ease-in-out "
+                    src="https://duruthemes.com/demo/html/archsan/light/images/projects/04.jpg "
+                    alt=""
+                  />
+                </a>
+                <div className="absolute left-[40px] top-[80px]">
+                  <p className="mb-[5px] tracking-[3px] leading-7 font-normal text-[12px] text-[#c5a47e]">
+                    INTERIOR
+                  </p>
+                  <h3 className="text-2xl tracking-[3px] leading-7 font-normal mb-4 font-heading text-[#c5a47e]">
+                    <a href="">MODERN BEDROOM</a>
+                  </h3>
+                  <a
+                    href=""
+                    className="text- [#fff] font-normal relative pe-[40px] leading-7 inline-block ps-[80px] text-[16px]"
+                  >
+                    <span className="before:content-[''] w-[80px] h-[1px] inline-block absolute left-0 bottom-[15px] bg-[#1b1b1b]"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-6 resolute left-0 top-[652px] -mt-[140px]">
+              <div className="relative ps-[90px] pb-[90px] group">
+                <a href="#" className="text-[#c5a47e] ">
+                  <img
+                    className="w-full h-auto group-hover:scale-95 duration-500 ease-in-out "
+                    src="https://duruthemes.com/demo/html/archsan/light/images/projects/03.jpg"
+                    alt=""
+                  />
+                </a>
+                <div className="absolute left-[40px] top-[80px]">
+                  <p className="mb-[5px] tracking-[3px] leading-7 font-normal text-[12px] text-[#c5a47e]">
+                    EXTERIOR
+                  </p>
+                  <h3 className="text-2xl tracking-[3px] leading-7 font-normal mb-4 font-heading text-[#c5a47e]">
+                    <a href="">ULTRA BLACKHOME</a>
+                  </h3>
+                  <a
+                    href=""
+                    className="text- [#fff] font-normal relative pe-[40px] leading-7 inline-block ps-[80px] text-[16px]"
+                  >
+                    <span className="before:content-[''] w-[80px] h-[1px] inline-block absolute left-0 bottom-[15px] bg-[#1b1b1b]"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-6 resolute left-[570px] top-[965px]">
+              <div className="relative ps-[90px] pb-[90px] group">
+                <a href="#" className="text-[#c5a47e] ">
+                  <img
+                    className="w-full h-auto group-hover:scale-95 duration-500 ease-in-out "
+                    src="https://duruthemes.com/demo/html/archsan/light/images/projects/05.jpg"
+                  />
+                </a>
+                <div className="absolute left-[40px] top-[80px]">
+                  <p className="mb-[5px] tracking-[3px] leading-7 font-normal text-[12px] text-[#c5a47e]">
+                  INTERIOR
+                  </p>
+                  <h3 className="text-2xl tracking-[3px] leading-7 font-normal mb-4 font-heading text-[#c5a47e]">
+                    <a href="">MODERN FOOD TABLE</a>
+                  </h3>
+                  <a
+                    href=""
+                    className="text- [#fff] font-normal relative pe-[40px] leading-7 inline-block ps-[80px] text-[16px]"
+                  >
+                    <span className="before:content-[''] w-[80px] h-[1px] inline-block absolute left-0 bottom-[15px] bg-[#1b1b1b]"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-6 resolute left-0 top-[1097px] -mt-[140px]">
+              <div className="relative ps-[90px] pb-[90px] group">
+                <a href="#" className="text-[#c5a47e] ">
+                  <img
+                    className="w-full h-auto group-hover:scale-95 duration-500 ease-in-out "
+                    src="https://duruthemes.com/demo/html/archsan/light/images/projects/01.jpg"
+                    alt=""
+                  />
+                </a>
+                <div className="absolute left-[40px] top-[80px]">
+                  <p className="mb-[5px] tracking-[3px] leading-7 font-normal text-[12px] text-[#c5a47e]">
+                  URBAN
+                  </p>
+                  <h3 className="text-2xl tracking-[3px] leading-7 font-normal mb-4 font-heading text-[#c5a47e]">
+                    <a href="">MOUNTAIN HOUSES</a>
+                  </h3>
+                  <a
+                    href=""
+                    className="text- [#fff] font-normal relative pe-[40px] leading-7 inline-block ps-[80px] text-[16px]"
+                  >
+                    <span className="before:content-[''] w-[80px] h-[1px] inline-block absolute left-0 bottom-[15px] bg-[#1b1b1b]"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Project02;
